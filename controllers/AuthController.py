61b61e84b8ddb4c2de8bb2179d51f87dac3d9204
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-22 22:39:38 -0500

    feat(auth): 🔐 Add authentication controller

---
+++ controllers/AuthController.py
@@ -0,0 +1,18 @@
+from fastapi import HTTPException
+from services.AuthService import authenticate_responsible
+from utils.jwt_utils import create_access_token
+from config.db import SessionLocal
+
+def login_controller(login_req):
+    db = SessionLocal()  
+    responsible = authenticate_responsible(db, login_req.email, login_req.password)  
+    
+   
+    if not responsible:
+        raise HTTPException(status_code=401, detail="Credenciales inválidas")
+    
+ 
+    token = create_access_token({"userId": str(responsible.id)}) 
+    
+   
+    return {"token": token, "responsibleId": str(responsible.id)}
